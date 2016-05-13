f061a829d4768ea4a9ee19055462a3d135765121
David Brusius <brusiusdavid@gmail.com>
2016-05-13 16:49:44 -0300

    Add bootstrap flash messages helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,20 @@
 module ApplicationHelper
   def bootstrap_class_for(flash_type)
-    {
-      success: "alert-success", error: "alert-danger", alert: "alert-warning",
-      notice: "alert-info"
-    }[flash_type] || flash_type.to_s
+    bootstrap_alert_classes = {
+      success:  "alert-success",
+      error:    "alert-danger",
+      alert:    "alert-warning",
+      notice:   "alert-info"
+    }
+    bootstrap_alert_classes[flash_type] || flash_type.to_s
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type.to_sym)}") do
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type.to_sym)}") do
         concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
         concat message
-      end
+      end)
     end
     nil
   end
