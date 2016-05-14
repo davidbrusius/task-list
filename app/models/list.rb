8b295a66da236746b1a0174e84351f34aefea85b
David Brusius <brusiusdavid@gmail.com>
2016-05-14 14:53:02 -0300

    Validate presence of user when creating a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
+  validates :user, presence: true
   validates :subject, presence: true
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe List, type: :model do
 
   it { should belong_to(:user) }
   it { should have_many(:tasks) }
+  it { should validate_presence_of(:user) }
   it { should validate_presence_of(:subject) }
 end
