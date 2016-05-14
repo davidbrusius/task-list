02be055dc532900b220a9a8d2a33a82becf1f9f3
David Brusius <brusiusdavid@gmail.com>
2016-05-14 13:26:25 -0300

    Setup relations between list and user

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ActiveRecord::Base
+  belongs_to :user
   has_many :tasks, dependent: :destroy
 
   validates :subject, presence: true
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe List, type: :model do
     expect(FactoryGirl.create(:list)).to be_valid
   end
 
+  it { should belong_to(:user) }
   it { should have_many(:tasks) }
   it { should validate_presence_of(:subject) }
 end
