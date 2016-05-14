62e16c541ad6fb72bdff45aebc1ec362eaef2efd
David Brusius <brusiusdavid@gmail.com>
2016-05-14 12:25:30 -0300

    Validate list subject presence

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,3 @@
 class List < ActiveRecord::Base
+  validates :subject, presence: true
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe List, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:list)).to be_valid
   end
+
+  it { should validate_presence_of(:subject) }
 end
