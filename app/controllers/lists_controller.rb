2103cfe9a2818b5dd189d0545f864a9feb90ba32
David Brusius <brusiusdavid@gmail.com>
2016-05-14 13:34:49 -0300

    Scope list actions with current_user to prevent other users access to list that are not owned by them

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
   # GET /lists
   # GET /lists.json
   def index
-    @lists = List.all
+    @lists = current_user.lists.all
   end
 
   # GET /lists/1
@@ -24,7 +24,7 @@ class ListsController < ApplicationController
   # POST /lists
   # POST /lists.json
   def create
-    @list = List.new(list_params)
+    @list = current_user.lists.new(list_params)
 
     respond_to do |format|
       if @list.save
@@ -64,7 +64,7 @@ class ListsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_list
-      @list = List.find(params[:id])
+      @list = current_user.lists.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
