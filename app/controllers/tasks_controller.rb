6437e5d707549982d1f3fa49442bc513d7b6453c
David Brusius <brusiusdavid@gmail.com>
2016-05-15 12:23:15 -0300

    Create tasks via AJAX and show in the respective list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,7 @@ class TasksController < ApplicationController
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
+        format.js   { }
       else
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
