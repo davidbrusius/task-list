51bb3520286c07d8b833a555e846e2e03b23bed8
David Brusius <brusiusdavid@gmail.com>
2016-05-16 16:04:57 -0300

    Add test for list publicly accessible scope

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  it "has a valid factory" do
-    expect(FactoryGirl.create(:list)).to be_valid
-  end
-
   it { should belong_to(:user) }
   it { should have_many(:tasks) }
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:subject) }
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:list)).to be_valid
+  end
+
+  describe '.publicly_accessible' do
+    it 'return only public lists' do
+      user = FactoryGirl.create(:user)
+      2.times { FactoryGirl.create(:list, user: user) }
+      3.times { FactoryGirl.create(:list, user: user, public_access: true) }
+
+      expect(List.publicly_accessible.count).to be(3)
+    end
+  end
 end
