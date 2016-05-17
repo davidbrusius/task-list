7d1d240af66a7f2a6512dd43e485ed79ac09210d
David Brusius <brusiusdavid@gmail.com>
2016-05-17 13:48:46 -0300

    Exclude current user list from the public lists query

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,9 @@ class ListsController < ApplicationController
   # GET /lists.json
   def index
     if params[:list_scope] == 'public'
-      @lists = List.publicly_accessible.includes(:tasks)
+      @lists = List.publicly_accessible
+                   .where.not(user: current_user)
+                   .includes(:tasks)
     else
       @lists = current_user.lists.includes(:tasks)
     end
