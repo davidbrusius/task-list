96c09bc45b32db667092aae3c6e2c6b87fddc63a
David Brusius <brusiusdavid@gmail.com>
2016-05-17 18:00:55 -0300

    Alias to scoped lists path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tasks
   resources :lists do
-    get ':list_scope', action: :index, on: :collection,
+    get ':list_scope', action: :index, on: :collection, as: :scoped,
                        constraints: { list_scope: /private|public/ }
   end
 
