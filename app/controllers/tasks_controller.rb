56038433e0cf1e5c6f39f06ddde4d1f9d59b7b40
David Brusius <brusiusdavid@gmail.com>
2016-05-18 17:04:31 -0300

    Redirect to lists path after update task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to lists_path, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
