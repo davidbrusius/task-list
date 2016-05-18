c4c9a40967f7496df3c8bf1590ebdbc02021a824
David Brusius <brusiusdavid@gmail.com>
2016-05-18 09:21:35 -0300

    Delete tasks dynamically via AJAX

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -61,6 +61,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
+      format.js   { }
     end
   end
 
