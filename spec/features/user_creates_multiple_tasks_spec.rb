dbaa47d3dd810b6a56d851e4d5a3e1ff236b847a
David Brusius <brusiusdavid@gmail.com>
2016-05-19 17:19:01 -0300

    Feature spec user create multiple tasks in a list via AJAX

---
+++ spec/features/user_creates_multiple_tasks_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Users create multiple tasks' do
+  let(:user) { FactoryGirl.create(:user) }
+  given!(:list) { FactoryGirl.create(:list, user: user) }
+
+  scenario 'after create a list add tasks via AJAX', js: true do
+    sign_in_with user.email, user.password
+    visit lists_path
+    click_link list.subject
+    for i in 0..4
+      task_description = Faker::Lorem.words(2).join(' ')
+      fill_in 'task_description', with: task_description
+      click_button 'Create Task'
+      expect(page).to have_content(task_description)
+    end
+  end
+end
