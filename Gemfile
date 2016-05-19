56914c61474032bb51aae7c2776ca40c32820cf5
David Brusius <brusiusdavid@gmail.com>
2016-05-19 17:17:35 -0300

    Setup database cleaner and selenium driver to use with tests

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ end
 group :test do
   gem 'shoulda-matchers', '~> 3.1'
   gem 'capybara'
+  gem 'database_cleaner'
+  gem 'selenium-webdriver'
 end
 
 gem 'rails_12factor', group: :production
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -55,6 +55,32 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Clean up and initialize database before
+  # running test exmaples
+  config.before(:suite) do
+    # Truncate database to clean up garbage from
+    # interrupted or badly written examples
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    # Use really fast transaction strategy for all
+    # examples except `js: true` capybara specs
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+
+    # Start transaction
+    DatabaseCleaner.cleaning do
+
+      # Run example
+      example.run
+    end
+
+    load "#{Rails.root}/db/seeds.rb" if example.metadata[:js]
+
+    # Clear session data
+    Capybara.reset_sessions!
+  end
 end
 
 Shoulda::Matchers.configure do |config|
