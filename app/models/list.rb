317517d352560536e3b1abc2131506cb8d6cc541
David Brusius <david.brusius@plataformatec.com.br>
2016-07-13 13:40:57 -0300

    Setup on delete cascade for has_many relations rather than using dependent: :destroy

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :tasks, dependent: :destroy
-  has_many :favorite_lists, dependent: :destroy
+  has_many :tasks
+  has_many :favorite_lists
   has_many :favorited_by, through: :favorite_lists, source: :user
 
   validates :user, presence: true
